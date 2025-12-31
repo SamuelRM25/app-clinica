714d4294477ce03c21e4726510e24f459532e396
SamuelRM25 <124316227+SamuelRM25@users.noreply.github.com>
2025-12-31 00:15:23 -0600

    Delete docker-entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# docker-entrypoint.sh
-
-# Reemplazar $PORT en los archivos de configuración
-sed -i "s/\${PORT}/$PORT/g" /etc/apache2/ports.conf
-sed -i "s/\${PORT}/$PORT/g" /etc/apache2/sites-available/000-default.conf
-
-# Iniciar Apache
-exec apache2-foreground
